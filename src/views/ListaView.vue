e6810fb7150e111c1f474f55fa3a2bc0232140e5
MiriamNeoris <mgonzalezb11@alumnos.nebrija.es>
2023-04-25 17:34:13 +0200

    Recogida datos

--- src/views/ListaView.vue
+++ src/views/ListaView.vue
@@ -1,3 +1,42 @@
 <template>
-    <h1>Estamos en lista</h1>
+    <section>
+         <h1>Estamos en lista</h1>
+         <ul v-for="product in products" :key="product.type">
+            <li>{{ product.balance }}</li>
+            <li>{{ product.type }}</li>
+            <li>{{ product.number }}</li>
+
+         </ul>
+    </section>
 </template>
+
+<script>
+import {productsStore} from '../stores/products'
+    export default {
+        components:{
+
+        },
+        data() {
+            return {
+                products: []
+            }
+        },
+        methods: {
+        async getAllProducts() {
+            try {
+                const useProductStore = productsStore()
+                this.products = await useProductStore.fetchProducts()
+                console.log(this.products)
+                this.fetched = true
+            } catch (e) {
+                this.error = true
+            }
+        }
+    },
+    created(){
+        console.log(this.getAllProducts)
+        this.getAllProducts()
+    }
+}
+
+</script>
