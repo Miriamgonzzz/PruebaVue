c12f8b21c7edc428c960a6391b41a057b6bde643
MiriamNeoris <mgonzalezb11@alumnos.nebrija.es>
2023-05-08 18:04:42 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/views/ShowView.vue
+++ src/views/ShowView.vue
@@ -4,7 +4,11 @@
       <CParagraph text="Mostrar"></CParagraph>
     </template>
     <template #content>
+<<<<<<< Updated upstream
       <CElements :src="product"></CElements>
+=======
+      <CElements :products="products"></CElements>
+>>>>>>> Stashed changes
     </template>
   </l-showForm>
 </template>
@@ -19,6 +23,7 @@ export default {
   components: { LShowForm, CParagraph, CElements },
   data() {
     return {
+<<<<<<< Updated upstream
       product: []
     }
   },
@@ -27,6 +32,23 @@ export default {
     const useProductStore = productsStore()
     const selectedProduct = await useProductStore.getProductById(id)
     this.product = selectedProduct
+=======
+      products: []
+    }
+  },
+  created() {
+    const id = this.$route.params.id
+    console.log(id)
+    const useProductStore = productsStore()
+    const selectedProducts = useProductStore.products.find((products) => products.mal_id == id)
+    this.products = selectedProducts
+    console.log(selectedProducts)
+    console.log(this.products)
+
+    //window.history.pushState(null, null, `/lista`)
+    //window.history.replaceState(null, null, `/lista`)
+    //this.$router.go(-1)
+>>>>>>> Stashed changes
   }
 }
 </script>
