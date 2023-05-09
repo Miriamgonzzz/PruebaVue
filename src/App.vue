b4816c0224b3065cf2849b303e73bf6b2aee6fa3
MiriamNeoris <mgonzalezb11@alumnos.nebrija.es>
2023-05-09 17:59:20 +0200

    eslimos

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,15 @@ import '@/assets/styles/style.scss'
 
 <template>
   <header class="v-app__header" v-if="$route.path !== '/'">
-    <p class="v-app__paragraph">AnimeFun</p> 
-    <div class="v-app__line"></div>   
+    <p class="v-app__paragraph">AnimeFun</p>
+    <div class="v-app__line"></div>
     <nav class="v-app__navbar">
       <ul class="v-app__ul">
-        <li class="v-app__li" :style="start"><RouterLink to="/start" class="v_app__router">Inicio</RouterLink></li>
-        <li class="v-app__li" :style="lista"><RouterLink to="/lista" class="v_app__router">Animes</RouterLink></li>
-        <li class="v-app__li" :style="form"><RouterLink to="/form" class="v_app__router">Formulario</RouterLink></li>
+        <li class="v-app__li" :style="start"><a href="/start" class="v_app__router">Inicio</a></li>
+        <li class="v-app__li" :style="lista"><a href="/lista" class="v_app__router">Animes</a></li>
+        <li class="v-app__li" :style="form">
+          <a href="/form" class="v_app__router">Formulario</a>
+        </li>
       </ul>
     </nav>
   </header>
@@ -23,78 +25,77 @@ import '@/assets/styles/style.scss'
 <script>
 export default {
   data() {
-    return {
-    }
+    return {}
   },
   computed: {
-   start(){
+    start() {
       if (this.$route.path === '/start') {
-        return 'background-color: rgba(144, 124, 144,56);';
+        return 'background-color: rgba(144, 124, 144,56);'
       }
-   },
-   lista(){
-    if (this.$route.path === '/lista'){
-        return 'background-color: rgba(144, 124, 144,56);';
+    },
+    lista() {
+      if (this.$route.path === '/lista') {
+        return 'background-color: rgba(144, 124, 144,56);'
       }
-   },
-   form(){
-     if (this.$route.path === '/form'){
-        return 'background-color: rgba(144, 124, 144,56);';
+    },
+    form() {
+      if (this.$route.path === '/form') {
+        return 'background-color: rgba(144, 124, 144,56);'
       }
-   }
-      
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
-.v-app__header{
+.v-app__header {
   margin: 0;
   width: 100%;
-  background:var(--color-background-showForm) ;
-  box-shadow: 0px 10px 5px var( --color-shadow) ;
+  background: var(--color-background-showForm);
+  box-shadow: 0px 10px 5px var(--color-shadow);
 }
 //rgb(185, 239, 248)
 
-.v-app__line{
+.v-app__line {
   width: 100%;
   height: 1px;
-  background:var(--color-shadow) ;
-  
+  background: var(--color-shadow);
 }
-.v-app__paragraph{
+.v-app__paragraph {
   margin: 0;
   padding: 10px;
   font-size: 3.5rem;
   text-align: center;
 }
-.v-app__ul{ 
+.v-app__ul {
   margin: 0;
   padding: 0;
   width: 100%;
+  height: 50px;
   display: flex;
   flex-direction: row;
   //gap: 20px;
   align-items: center;
-
- 
 }
-.v-app__li{
+.v-app__li {
   width: 100%;
-  list-style: none;  
+  height: 100%;
+  list-style: none;
   text-align: center;
   justify-content: center;
   align-items: center;
 }
-.v_app__router{
+.v_app__router {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
   font-size: 30px;
   color: black;
   text-decoration: none;
 }
-.v-app__li:hover{
+.v-app__li:hover {
   background: var(--color-background-button);
 }
-
-
 </style>
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,21 +1,73 @@
 <template>
-    <CParagraph text="Explora el Universo del Anime"></CParagraph>
-    <SpiritElement/>
+  <CParagraph text="Explora el Universo del Anime"></CParagraph>
+  <SpiritElement
+    class="v-spiritElement__water"
+    :beforeTop="water.beforeTop"
+    :beforeColor="water.beforeColor"
+  />
+  <SpiritElement
+    class="v-spiritElement__fire"
+    :background="fire.background"
+    :gradient="fire.gradient"
+    :eyeD="fire.eyeD"
+    :eyeI="fire.eyeI"
+    :beforeTop="fire.beforeTop"
+    :beforeColor="fire.beforeColor"
+  />
+  <SpiritElement
+    class="v-spiritElement__earth"
+    :background="earth.background"
+    :gradient="earth.gradient"
+    :eyeD="earth.eyeD"
+    :eyeI="earth.eyeI"
+  />
+  <SpiritElement
+    class="v-spiritElement__electric"
+    :background="electric.background"
+    :gradient="electric.gradient"
+    :eyeD="electric.eyeD"
+    :eyeI="electric.eyeI"
+    :beforeColor="electric.beforeColor"
+  />
 </template>
 
 <script>
 import CParagraph from '../components/c-paragraph.vue'
 import SpiritElement from '../components/c-spiritElement.vue'
 
-    export default {
-        components: {
-            CParagraph,
-            SpiritElement
-        },
-        data() {
-            return {
-            }
-        }
+export default {
+  components: {
+    CParagraph,
+    SpiritElement
+  },
+  data() {
+    return {
+      water: {
+        beforeTop: '31%',
+        beforeColor: 'rgb(224, 162, 28)'
+      },
+      fire: {
+        background: 'red',
+        gradient: 'linear-gradient(to bottom, rgb(255, 51, 0),rgb(255, 123, 0))',
+        eyeD: 'rgb(255, 123, 0)',
+        eyeI: 'rgb(255, 123, 0)',
+        beforeTop: '65%',
+        beforeColor: 'rgb(224, 162, 28)'
+      },
+      earth: {
+        background: 'rgb(58, 31, 5)',
+        gradient: 'linear-gradient(to bottom,rgb(91, 180, 27),rgb(42, 46, 40))',
+        eyeD: 'rgb(91, 180, 27)',
+        eyeI: 'rgb(91, 180, 27)'
+      },
+      electric: {
+        background: 'rgb(251, 255, 0)',
+        gradient: 'linear-gradient(to bottom, rgb(223, 201, 8), rgb(224, 162, 28))',
+        eyeD: 'rgb(224, 162, 28)',
+        eyeI: 'rgb(224, 162, 28)',
+        beforeColor: 'rgb(91, 180, 27)'
+      }
     }
-
+  }
+}
 </script>
