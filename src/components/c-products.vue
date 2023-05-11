55d334ecad551971ec924e666653da45e0c71c50
Daniel López Martínez <daniel.lopezm@neoris.com>
2023-05-11 11:49:49 +0200

    limo fix

--- src/components/c-products.vue
+++ src/components/c-products.vue
@@ -1,63 +1,65 @@
 <template>
-    <div class="c-product">
-        <img class="c-product__img" :src="product.image_url" :title="product.animeTitle" :alt="alt" @mouseover="mostrarSpam= true" @mouseleave="mostrarSpam=false">
-        <span v-if="mostrarSpam" class="c-product__paragraph">{{ product.title}}</span>
-    </div>
+  <div class="c-product" @mouseover="mostrarSpam = true" @mouseleave="mostrarSpam = false">
+    <img class="c-product__img" :src="product.image_url" :title="product.animeTitle" :alt="alt" />
+    <span v-if="mostrarSpam" class="c-product__paragraph">{{ product.title }}</span>
+  </div>
 </template>
 
 <script>
-
-export default{
-    name: 'CImg',
-    props:{
-        src:String,
-        alt:String,
-        product:{
-            type:Object,
-            required:true
-        }
-    },
-   data(){
-    return{
-        mostrarSpam:false
+export default {
+  name: 'CImg',
+  props: {
+    src: String,
+    alt: String,
+    product: {
+      type: Object,
+      required: true
     }
-   },
+  },
+  data() {
+    return {
+      mostrarSpam: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.c-product {
+  max-width: 400px;
+  max-height: 600px;
 
-.c-product{
-    text-align: center;
-   display: inline-block;
-    position: relative;
-    
-}
-.c-product__img{
-    width:100%;
-    height:100%;
-    object-fit:cover;
-
+  position: relative;
+  overflow: hidden;
+  border-radius: 40px;
+  transition: 0.2s all;
 }
 
-.c-product__img:hover{
-    //opacity: 0.3;
-    border-radius:50%;
-    box-shadow: 0px 0px 15px 15px var(--color-shadow);
-    transform: rotate(360deg);
+.c-product:hover {
+  cursor: pointer;
+  //opacity: 0.3;
+  box-shadow: 0px 0px 15px 15px var(--color-shadow);
 }
-.c-product__paragraph{
-    padding: 5px;
-    font-size: 20px;
-    background: rgb(255, 255, 255);
-    border-radius: 10px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+.c-product__img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
-</style> 
-
-
- 
+// .c-product__img:hover {
+//   //opacity: 0.3;
+//   border-radius: 50%;
+//   box-shadow: 0px 0px 15px 15px var(--color-shadow);
+//   transform: rotate(360deg);
+// }
+.c-product__paragraph {
+  padding: 5px;
+  font-size: 20px;
+  background: rgb(255, 255, 255);
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,6 +1,19 @@
 <template>
   <CParagraph text="Explora el Universo del Anime"></CParagraph>
-  <div class="peito">
+
+  <section class="v-start__wrapper">
+    <article
+      v-for="(limo, index) in limos"
+      :key="index"
+      class="v-start__limo"
+      :class="limo.inverted ? '--inverted' : ''"
+    >
+      <SpiritElement class="v-start__limo--spirit" />
+      <CBocadillo class="v-start__limo--text" />
+    </article>
+  </section>
+
+  <!-- <div class="peito">
     <SpiritElement
       class="v-spiritElement__water"
       :backgroundColor="water.backgroundColor"
@@ -37,13 +50,44 @@
       :shadowColor="electric.shadow"
     />
     <CBocadillo :right="bocadilloIzq.right" :rotate="bocadilloIzq.rotate" />
-  </div>
+  </div> -->
 </template>
 
 <script>
 import CParagraph from '../components/c-paragraph.vue'
 import CBocadillo from '../components/c-bocadillo.vue'
 import SpiritElement from '../components/c-spiritElement.vue'
+
+const limos = [
+  { backgroundColor: 'rgb(137, 209, 231)', shadow: 'rgba(255, 255, 255, 0.8)' },
+  {
+    background: 'red',
+    gradient: 'linear-gradient(to bottom, rgb(255, 51, 0),rgb(255, 123, 0))',
+    eyeD: 'rgb(255, 123, 0)',
+    eyeI: 'rgb(255, 123, 0)',
+    backgroundColor: 'rgb(243, 126, 90)',
+    shadow: 'rgb(255, 255, 255)',
+    inverted: true
+  },
+  {
+    background: 'rgb(58, 31, 5)',
+    gradient: 'linear-gradient(to bottom,rgb(91, 180, 27),rgb(42, 46, 40))',
+    eyeD: 'rgb(91, 180, 27)',
+    eyeI: 'rgb(91, 180, 27)',
+    beforeTop: '75%',
+    backgroundColor: 'rgb(116, 235, 4)',
+    shadow: 'rgb(255, 255, 255)'
+  },
+  {
+    background: 'rgb(255, 136, 0)',
+    gradient: 'linear-gradient(to bottom, rgb(223, 201, 8), rgb(224, 162, 28))',
+    eyeD: 'rgb(224, 162, 28)',
+    eyeI: 'rgb(224, 162, 28)',
+    backgroundColor: 'rgb(231, 255, 13)',
+    shadow: 'rgb(255, 255, 255)',
+    inverted: true
+  }
+]
 
 export default {
   components: {
@@ -53,72 +97,51 @@ export default {
   },
   data() {
     return {
-      water: {
-        backgroundColor: 'rgb(137, 209, 231)',
-        shadow: 'rgba(255, 255, 255, 0.8)'
-      },
-      fire: {
-        background: 'red',
-        gradient: 'linear-gradient(to bottom, rgb(255, 51, 0),rgb(255, 123, 0))',
-        eyeD: 'rgb(255, 123, 0)',
-        eyeI: 'rgb(255, 123, 0)',
-        backgroundColor: 'rgb(243, 126, 90)',
-        shadow: 'rgb(255, 255, 255)'
-      },
-      earth: {
-        background: 'rgb(58, 31, 5)',
-        gradient: 'linear-gradient(to bottom,rgb(91, 180, 27),rgb(42, 46, 40))',
-        eyeD: 'rgb(91, 180, 27)',
-        eyeI: 'rgb(91, 180, 27)',
-        beforeTop: '75%',
-        backgroundColor: 'rgb(116, 235, 4)',
-        shadow: 'rgb(255, 255, 255)'
-      },
-      electric: {
-        background: 'rgb(255, 136, 0)',
-        gradient: 'linear-gradient(to bottom, rgb(223, 201, 8), rgb(224, 162, 28))',
-        eyeD: 'rgb(224, 162, 28)',
-        eyeI: 'rgb(224, 162, 28)',
-        backgroundColor: 'rgb(231, 255, 13)',
-        shadow: 'rgb(255, 255, 255)'
-      },
-      bocadilloDer: {
-        left: ' -20px',
-        rotate: '90deg'
-      },
-      bocadilloIzq: {
-        right: ' -20px',
-        rotate: '270deg'
-      }
+      limos: limos
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.peito {
-  margin: 10px;
-  display: grid;
-  grid-template-columns: repeat(3, 15% 70% 15%);
-  gap: 10px;
-  border: 1px solid rgb(137, 209, 231);
+.v-start__wrapper {
+  padding-bottom: 50px;
+  width: 98%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
   align-items: center;
 }
-.v-spiritElement__water {
-  grid-column: 1;
-  grid-row: 1;
-}
-.v-spiritElement__fire {
-  grid-column: 3;
-  grid-row: 2;
-}
-.v-spiritElement__earth {
-  grid-column: 1;
-  grid-row: 3;
+
+.v-start__limo {
+  position: relative;
+  height: 100%;
+  width: 90%;
+  display: flex;
+  gap: 60px;
+  flex-direction: row;
+  align-items: center;
 }
 
-.v-spiritElement__electric {
-  grid-column: 3;
-  grid-row: 4;
+.v-start__limo--spirit {
+}
+
+.v-start__limo--text {
+}
+
+.--inverted {
+  flex-direction: row-reverse;
+}
+
+@media only screen and (max-width: 900px) {
+  .v-start__limo--spirit {
+    position: absolute;
+    top: -90px;
+    left: -80px;
+    transform: scale(0.2);
+    z-index: 999;
+  }
 }
 </style>
